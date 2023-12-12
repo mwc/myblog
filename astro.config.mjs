afe3b76d04a4aeafcd99fc728be92a3303a41537
前端子鱼 <mwc@foxmail.com>
2023-12-12 22:14:58 +0800

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), react(), vue(), tailwind()]
+});
