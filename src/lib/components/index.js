0b17b990010874b8dd3ec4a95df250c1352543fa
前端子鱼 <mwc@foxmail.com>
2023-12-19 20:38:35 +0800

    chore: improve link style.

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,3 +1,4 @@
 export { default as Subject } from './subject.astro'
 export { default as ArticleList } from './articles.astro'
+export { default as ArticlePage } from './articlePage.astro'
 export { default as Tags } from './tags.astro'
