c582a171d4492b4551e07221ea87ca4429965951
前端子鱼 <mwc@foxmail.com>
2023-12-20 20:54:11 +0800

    feat: add tags page

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,4 +1,2 @@
 export { default as Subject } from './subject.astro'
-export { default as ArticleList } from './articles.astro'
-export { default as ArticlePage } from './articlePage.astro'
-export { default as Tags } from './tags.astro'
+export * as data from './posts.astro'
