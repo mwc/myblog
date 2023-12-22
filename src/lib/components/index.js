d2dc23fdab3f35a473fffd0a481216cfe7a46555
前端子鱼 <mwc@foxmail.com>
2023-12-22 21:11:24 +0800

    fix: paginate

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,3 +1,4 @@
 export { default as Subject } from './subject.astro'
 export { default as Pager } from './pager.astro'
+export { default as Page } from './page.astro'
 export * as data from './data.astro'
