ae801d4c51faae4668475591270bf15e365229d2
前端子鱼 <mwc@foxmail.com>
2023-12-25 17:31:03 +0800

    feat: support mdx and expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), react(), vue(), tailwind()],
+  integrations: [svelte(), react(), vue(), tailwind(), expressiveCode(), mdx()],
   site: "https://mwc.github.io",
   base: "/myblog/",
   trailingSlash: "always",
-  prefetch: true,
+  prefetch: true
 });
