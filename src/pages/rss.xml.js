9f0362e85d77ad8836e4c0b0144af5292997c62a
前端子鱼 <mwc@foxmail.com>
2023-12-31 19:49:30 +0800

    fix: update rss and schma

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
-export const GET = async (context) => rss({
-    title: "ZiYu's blog",
-    description: "ZiYu's blog",
-    site: context.site + context.base,
-    items: await pagesGlobToRssItems(import.meta.glob('/src/content/posts/**/*.{md,mdx}')),
-})
+export const GET = async (context) => {
+    const posts = await getCollection('posts')
+
+    return rss({
+        title: "ZiYu's blog",
+        description: "ZiYu's blog",
+        site: context.site,
+        items: posts.map(post => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: './myblog/' + post.data.link ?? ''
+        }))
+    })
+}
--- src/schmas/posts.js
+++ src/schmas/posts.js
@@ -1,18 +1,19 @@
 import { z, defineCollection } from 'astro:content'
+const { object, string, array, number } = z
 
 export default defineCollection({
     type: 'content',
-    schema: z.object({
-        title: z.string(),
-        pubDate: z.string().transform(str => new Date(str)),
-        description: z.string(),
-        author: z.string(),
-        link: z.string(),
-        cover: z.string(),
-        tag: z.array(z.string()),
-        readingTime: z.object({
-            words: z.number(),
-            minutes: z.number()
+    schema: object({
+        title: string(),
+        pubDate: string().transform(str => new Date(str)),
+        description: string(),
+        author: string(),
+        link: string(),
+        cover: string(),
+        tag: array(string()),
+        readingTime: object({
+            words: number(),
+            minutes: number()
         }).optional()
     })
 })
